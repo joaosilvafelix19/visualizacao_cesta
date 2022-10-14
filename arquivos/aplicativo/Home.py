f27a3e82814d3835c0efca0ca387faa65fd50a46
João Felix <joaosilvafelix19@gmail.com>
2022-10-14 11:45:03 -0300

    atualizacao_texto

--- arquivos/aplicativo/Home.py
+++ arquivos/aplicativo/Home.py
@@ -26,7 +26,7 @@ st.title('Introdução')
 
 st.markdown("A coleta de informações de forma automática por meio de dados disponíveis na internet vem crescendo continuamente e rapidamente para uma ampla gama de produtos e serviços, incluindo a coleta de preços de produtos alimentícios. Junto a esta maior disponibilidade de preços sobre produtos em aplicativos ou em web pages, também há novas formas de coleta de dados por meio de tecnologias como web scraping, criando-se, desta forma, possibilidades para que se possa construir novos índices de preços ou seguir metodologias de órgãos estatísticos oficiais com dados disponíveis on-line.")
 
-st.markdown("Dada esta possibilidade, esse aplicativo tem o propósito de acompanhar e exibir algumas informações sobre os custos de uma cesta de produtos alimentícios para o município de João Pessoa, diariamente e de forma automática. Inicialmente, o acompanhamento das alterações nos preços desta cesta de bens será limitado a região metropolitana da capital do Estado da Paraíba, podendo ter sua análise ampliada em um momento futuro para mais capitais brasileiras.")
+st.markdown("Dada esta possibilidade, esse aplicativo tem o propósito de acompanhar e exibir algumas informações sobre os custos de uma cesta de produtos alimentícios para o município de João Pessoa, sendo atualizado diariamente e de forma automática. Inicialmente, o acompanhamento das alterações nos preços desta cesta de bens será limitado a região metropolitana da capital do Estado da Paraíba, podendo ter sua análise ampliada em um momento futuro para mais capitais brasileiras e mais itens.")
 
 st.markdown("Os bens escolhidos para o acompanhamento e estimação do custo da cesta básica são os indicados pelo decreto lei n° 399/1938 que, entre outras coisas, estabelece quais itens serão considerados para a composição da cesta em cada região do país e a quantidade mínima que cada adulto deve consumir por mês.")
 
@@ -34,7 +34,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.write("""
-             Por fim, ressalta-se que este aplicativo foi desenvolvido pelo Laboratório de Inteligência Artificial e Macroeconomia Computacional (LABIMEC-UFPB), que tem como objetivo realizar pesquisas voltadas para a análise de políticas macroeconômicas.
+             Por fim, ressalta-se que este aplicativo foi desenvolvido pelo Laboratório de Inteligência Artificial e Macroeconomia Computacional (LABIMEC-UFPB) e é certificado pelo Instituto Nacional da Propriedade Industrial (INPI).
              
              Caso queira ficar atualizado sobre informações sobre o custo da cesta básica e outras informações sobre assuntos econômicos, nos siga nas nossas redes sociais ou nos envie um e-mail: 
              
