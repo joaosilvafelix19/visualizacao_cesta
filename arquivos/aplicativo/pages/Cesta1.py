ab2f4ba349b2f33336fe4f242d64fed20e355b7c
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 16:04:35 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -7,12 +7,12 @@ import os
 st.title("Cesta Básica em João Pessoa - *LABIMEC*")
 
 # Importando os dados
-data_path = "./dados"  # Relative path
+data_path = "C:/Users/joaos/Documents/GitHub/visualizacao_cesta/arquivos/aplicativo/dados"  # Relative path
 
 try:
     os.chdir(data_path)
     # Load your data here, for example:
-    # df = pd.read_csv("your_data_file.csv")
+    df = pd.read_csv("dados_jp.xlsx")
     st.write("Dados carregados com sucesso!")
 except FileNotFoundError:
     st.error("O caminho especificado não foi encontrado. Verifique se o diretório 'dados' existe no caminho relativo.")
