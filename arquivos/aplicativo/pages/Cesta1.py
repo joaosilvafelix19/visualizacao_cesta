1a0ed90c0cb5462a32317213c120085fa4e07584
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 16:25:56 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import pandas as pd
+import requests
 
 st.title("Cesta Básica em João Pessoa - *LABIMEC*")
 
-# Correct URL to the raw Excel file in GitHub
-url = 'https://raw.githubusercontent.com/joaosilvafelix19/visualizacao_cesta/main/arquivos/aplicativo/dados/dados_jp.xlsx'
+# URL to the raw Excel file in GitHub using GitHub API
+url = 'https://api.github.com/repos/joaosilvafelix19/visualizacao_cesta/contents/arquivos/aplicativo/dados/dados_jp.xlsx'
 
-try:
-    # Read the Excel file directly from GitHub
-    df = pd.read_excel(url)
+# Send request to GitHub API to get file content
+response = requests.get(url)
+if response.status_code == 200:
+    file_content = response.json()['content']
+    file_content = pd.io.common.get_handle(file_content, 'rb').read()
+    
+    # Load the content into a pandas DataFrame
+    df = pd.read_excel(pd.io.common.BytesIO(file_content))
     st.write("Dados carregados com sucesso!")
     st.write(df.head())  # Display the first few rows of the dataframe
-except Exception as e:
-    st.error(f"Ocorreu um erro ao carregar os dados: {e}")
+else:
+    st.error(f"Ocorreu um erro ao carregar os dados: HTTP {response.status_code} - {response.reason}")
