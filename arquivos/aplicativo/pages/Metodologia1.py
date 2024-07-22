94f5cd5b2816c2a9dac94a9fe6461735cf2b9c0a
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 20:48:26 -0300

    Update Metodologia1.py

--- arquivos/aplicativo/pages/Metodologia1.py
+++ arquivos/aplicativo/pages/Metodologia1.py
@@ -1,15 +1,36 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 from st_aggrid import AgGrid
-
 import os
 
-# Caminho relativo
+# Path to the Excel file in your GitHub repository
 relative_path = 'arquivos/aplicativo/dados/dados_jp.xlsx'
 
-# Caminho absoluto
+# Construct the absolute path based on the current working directory
 file_path = os.path.join(os.getcwd(), relative_path)
 
+# Print the file path to verify it
 st.write(f"File path: {file_path}")
 
+# Function to load data
+@st.cache_data
+def load_data():
+    try:
+        # Attempt to read the Excel file
+        data = pd.read_excel(file_path)
+        return data
+    except Exception as e:
+        # Print error message if file reading fails
+        st.error(f"Error loading the Excel file: {e}")
+        return pd.DataFrame()  # Return an empty DataFrame in case of error
+
+# Load the data
+data = load_data()
+
+# Check if data is loaded successfully
+if not data.empty:
+    # Display the data using AgGrid
+    st.title('Data Viewer')
+    AgGrid(data)
+else:
+    st.write("No data to display.")
