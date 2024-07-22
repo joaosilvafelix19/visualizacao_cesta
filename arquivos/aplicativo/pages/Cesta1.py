0f82ea4f4b9b480b90159142e3b9fd827053becb
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 16:21:42 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("Cesta Básica em João Pessoa - *LABIMEC*")
 
 # URL to the raw Excel file in GitHub
-url = 'https://raw.githubusercontent.com/joaosilvafelix19/visualizacao_cesta/main/arquivos/aplicativo/dados/dados_jp.xlsx'
+url = 'https://github.com/joaosilvafelix19/visualizacao_cesta/blob/main/arquivos/aplicativo/dados/dados_jp.xlsx'
 
 try:
     # Read the Excel file directly from GitHub
