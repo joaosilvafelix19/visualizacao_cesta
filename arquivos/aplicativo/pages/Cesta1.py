863d4dbad5d9465e6c87c0473c4040bc74a59548
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 19:36:54 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-import plotly.express as px
+#import plotly.express as px
 #import plotly.graph_objects as go
 
 # Path and file name
