5faa2b5a51b8efbb340a4b68b3e090aa5c269a26
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 20:06:59 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -4,8 +4,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 import os
 
-# Caminho absoluto para a pasta "dados"
-path = r'C:/Users/joaos/Documents/GitHub/visualizacao_cesta/arquivos/aplicativo/dados'
+# Obter o caminho absoluto para a pasta "dados"
+path = os.path.abspath('dados')
 
 # Nome do arquivo Excel
 file_name = 'dados_jp.xlsx'
@@ -13,8 +13,3 @@ file_name = 'dados_jp.xlsx'
 # Combinar o caminho com o nome do arquivo para obter o caminho completo
 excel_file = os.path.join(path, file_name)
 
-# Exibir o caminho completo do arquivo
-st.write("Caminho completo do arquivo Excel:", excel_file)
-
-# Ler o Excel em um DataFrame
-cesta = pd.read_excel(excel_file)
