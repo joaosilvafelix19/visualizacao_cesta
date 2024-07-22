49ddb1bf411ba599b7123e62ed13ecbd7b0c9ea0
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 19:54:06 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -6,3 +6,12 @@ import os
 
 # Obter o caminho absoluto para a pasta "dados"
 path = os.path.abspath('dados')
+
+# Nome do arquivo Excel
+file_name = 'dados_jp.xlsx'
+
+# Combinar o caminho com o nome do arquivo para obter o caminho completo
+excel_file = os.path.join(path, file_name)
+
+# Importando os dados
+pnad = pd.read_excel(excel_file)
