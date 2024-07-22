5d09caef930ee1c57dc75e35f3680ef9267ca320
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 18:17:41 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -1,21 +1,20 @@
 import streamlit as st
 import pandas as pd
-import requests
+import os
+import plotly.express as px
+import plotly.graph_objects as go
 
-st.title("Cesta Básica em João Pessoa - *LABIMEC*")
+# Importando os dados e fazendo algumas manipulações
 
-# URL to the raw Excel file in GitHub using GitHub API
-url = 'https://api.github.com/repos/joaosilvafelix19/visualizacao_cesta/contents/arquivos/aplicativo/dados/dados_jp.xlsx'
-
-# Send request to GitHub API to get file content
-response = requests.get(url)
-if response.status_code == 200:
-    file_content = response.json()['content']
-    file_content = pd.io.common.get_handle(file_content, 'rb').read()
-    
-    # Load the content into a pandas DataFrame
-    df = pd.read_excel(pd.io.common.BytesIO(file_content))
-    st.write("Dados carregados com sucesso!")
-    st.write(df.head())  # Display the first few rows of the dataframe
+# Definindo diretório
+root = os.getcwd()
+if root[0] == '/':
+    root = '/GitHub/visualizacao_cesta/'
 else:
-    st.error(f"Ocorreu um erro ao carregar os dados: HTTP {response.status_code} - {response.reason}")
+    root = os.path.abspath('../..')
+path = '/arquivos/aplicativo/dados'
+
+# Importando os dados da cesta
+cesta = pd.read_excel(f"{root}{path}/dados_jp.xlsx")
+df = cesta.iloc[:,[6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50]]
+df30 = df.tail(1)
