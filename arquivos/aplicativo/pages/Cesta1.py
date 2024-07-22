a0c2f60cbcc72cbd6cafe1620da733ec51d2439c
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 19:23:12 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -4,3 +4,9 @@ import os
 
 # Obter o caminho absoluto para a pasta "dados"
 path = os.path.abspath('dados')
+
+
+# Importando os dados da cesta
+cesta = pd.read_excel(f"{root}{path}/dados_jp.xlsx")
+df = cesta.iloc[:,[6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50]]
+df30 = df.tail(1)
