af1021d81ada1c6b5e34bc1ef18b9a52d834bc55
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 20:45:35 -0300

    Update Metodologia1.py

--- arquivos/aplicativo/pages/Metodologia1.py
+++ arquivos/aplicativo/pages/Metodologia1.py
@@ -11,10 +11,3 @@ relative_path = 'arquivos/aplicativo/dados/dados_jp.xlsx'
 # Caminho absoluto
 file_path = os.path.join(os.getcwd(), relative_path)
 
-# Checa se o caminho existe
-if os.path.exists(file_path):
-    file = Image.open(file_path)
-else:
-    st.error(f"Image file not found at {file_path}")
-    file = None
-
