bbdba5b0c3209ed0df722470cb8230127954ed89
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 19:57:45 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -4,14 +4,21 @@ import plotly.express as px
 import plotly.graph_objects as go
 import os
 
-# Obter o caminho absoluto para a pasta "dados"
+# Obtain the absolute path for the "dados" folder
 path = os.path.abspath('dados')
 
-# Nome do arquivo Excel
+# Name of the Excel file
 file_name = 'dados_jp.xlsx'
 
-# Combinar o caminho com o nome do arquivo para obter o caminho completo
+# Combine path and filename for the complete Excel file path
 excel_file = os.path.join(path, file_name)
 
-# Importando os dados
-cesta = pd.read_excel(excel_file)
+# Read the data from the Excel file
+try:
+  cesta = pd.read_excel(excel_file)
+except FileNotFoundError:
+  st.error("Error: Excel file not found. Please ensure 'dados_jp.xlsx' is in the 'dados' folder.")
+else:
+  # Display the first 10 rows of the DataFrame as a table
+  st.subheader("Data from 'dados_jp.xlsx'")
+  st.dataframe(cesta.head(10))
