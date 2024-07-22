2bbad18478e2b5e7d17aaa925443716e53a2f6a9
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 20:04:03 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -13,5 +13,8 @@ file_name = 'dados_jp.xlsx'
 # Combinar o caminho com o nome do arquivo para obter o caminho completo
 excel_file = os.path.join(path, file_name)
 
+# Exibir o caminho completo do arquivo
+st.write("Caminho completo do arquivo Excel:", excel_file)
+
 # Ler o Excel em um DataFrame
 cesta = pd.read_excel(excel_file)
