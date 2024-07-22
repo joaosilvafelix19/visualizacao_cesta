72bb4a6ca55421185f2c3300ede99ad6ec1ff421
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 19:20:36 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -4,17 +4,3 @@ import os
 import plotly.express as px
 import plotly.graph_objects as go
 
-# Importando os dados e fazendo algumas manipulações
-
-# Definindo diretório
-root = os.getcwd()
-if root[0] == '/':
-    root = '/GitHub/visualizacao_cesta/'
-else:
-    root = os.path.abspath('../..')
-path = '/arquivos/aplicativo/dados'
-
-# Importando os dados da cesta
-cesta = pd.read_excel(f"{root}{path}/dados_jp.xlsx")
-df = cesta.iloc[:,[6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50]]
-df30 = df.tail(1)
