bc04aa8cff2f45b67c623305be17d2fd13b8f802
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 20:47:05 -0300

    Update Metodologia1.py

--- arquivos/aplicativo/pages/Metodologia1.py
+++ arquivos/aplicativo/pages/Metodologia1.py
@@ -1,13 +1,22 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 from st_aggrid import AgGrid
-
 import os
 
-# Caminho relativo
+# Path to the Excel file in your GitHub repository
 relative_path = 'arquivos/aplicativo/dados/dados_jp.xlsx'
 
-# Caminho absoluto
+# Construct the absolute path based on the current working directory
 file_path = os.path.join(os.getcwd(), relative_path)
 
+# Load the Excel file into a DataFrame
+@st.cache_data
+def load_data():
+    return pd.read_excel(file_path)
+
+# Load the data
+data = load_data()
+
+# Display the data using AgGrid
+st.title('Data Viewer')
+AgGrid(data)
