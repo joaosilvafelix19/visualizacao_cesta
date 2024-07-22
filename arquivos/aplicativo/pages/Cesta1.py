1575814aeb06aa90a039d38f8de671c09ee422ac
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 20:17:23 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -4,30 +4,13 @@ import plotly.express as px
 import plotly.graph_objects as go
 import os
 
-# Define o caminho para o diretório onde o arquivo Excel está localizado
-# Certifique-se de ajustar o caminho conforme a estrutura do seu projeto
-path = "C:/Users/joaos/Documents/GitHub/visualizacao_cesta/arquivos/aplicativo/dados/dados_jp.xlsx"
-file_name = 'dados_jp.xlsx'
-excel_file = os.path.join(path, file_name)
-
-# Verifica o diretório atual e o caminho do arquivo para depuração
-st.write("Diretório atual:", os.getcwd())
-st.write("Caminho do arquivo:", excel_file)
-
-# Verifica se o arquivo existe no caminho especificado
-if not os.path.exists(excel_file):
-    st.error(f'O arquivo {excel_file} não foi encontrado.')
+# Definindo diretório
+root = os.getcwd()
+if root[0] == '/':
+    root = '/GitHub/visualizacao_cesta/'
 else:
-    try:
-        # Lê o arquivo Excel para um DataFrame
-        df = pd.read_excel(excel_file)
-        st.write(df)
+    root = os.path.abspath('../..')
+path = '/arquivos/aplicativo/dados'
 
-        # Exemplo de visualização com Plotly Express
-        if not df.empty:
-            st.subheader("Visualização dos Dados")
-            # Ajuste os nomes das colunas conforme a estrutura dos seus dados
-            fig = px.line(df, x=df.columns[0], y=df.columns[1], title="Gráfico dos Dados")
-            st.plotly_chart(fig)
-    except Exception as e:
-        st.error(f"Ocorreu um erro ao processar o arquivo: {e}")
+# Importando os dados dos preços não ponerados
+cesta = pd.read_excel(f"{root}{path}/dados_jp.xlsx", sheet_name="cesta")
