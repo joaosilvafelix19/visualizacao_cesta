10b380796f2681db0aa6cb321396c348a4e7a49d
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 19:53:01 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import os
 
 # Obter o caminho absoluto para a pasta "dados"
 path = os.path.abspath('dados')
