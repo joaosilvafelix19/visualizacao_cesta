5080108dd8cec7dfee931573e50a14db3095e2e7
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 21:45:18 -0300

    Update cesta1.py

--- arquivos/aplicativo/pages/cesta1.py
+++ arquivos/aplicativo/pages/cesta1.py
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 import os
 
 # Define a test path
-test_path = 'C:\\Users\\joaos\\Documents\\GitHub\\visualizacao_cesta\\arquivos\\aplicativo\\dados\\test_file.xlsx'
+test_path = 'C:\\Users\\joaos\\Documents\\GitHub\\visualizacao_cesta\\arquivos\\aplicativo\\dados\\dados_jp1.xlsx'
 
 # Check if the test file exists
-print(os.path.exists(test_path))
+st.write(print(os.path.exists(test_path)))
