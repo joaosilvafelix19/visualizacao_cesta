f13cd66d3c007273fd646cb1102595b5e18644a5
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 21:43:59 -0300

    Update cesta1.py

--- arquivos/aplicativo/pages/cesta1.py
+++ arquivos/aplicativo/pages/cesta1.py
@@ -3,13 +3,10 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-# Importando os dados e fazendo algumas manipulações
-file_path = "C:/Users/joaos/Documents/GitHub/visualizacao_cesta/arquivos/aplicativo/dados/dados_jp1.xlsx"
+import os
 
-# Check if the file exists
-if os.path.exists(file_path):
-    # Load the data from the Excel file
-    df = pd.read_excel(file_path)
-else:
-    st.error("The file was not found. Please check the file path.")
+# Define a test path
+test_path = 'C:\\Users\\joaos\\Documents\\GitHub\\visualizacao_cesta\\arquivos\\aplicativo\\dados\\test_file.xlsx'
 
+# Check if the test file exists
+print(os.path.exists(test_path))
