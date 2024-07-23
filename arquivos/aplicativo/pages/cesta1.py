188058a4a7e74a5261043608af877cdf3ebc37b8
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 21:37:30 -0300

    Update cesta1.py

--- arquivos/aplicativo/pages/cesta1.py
+++ arquivos/aplicativo/pages/cesta1.py
@@ -5,21 +5,11 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 # Importando os dados e fazendo algumas manipulações
-
-# Obter o caminho absoluto para a pasta "dados"
-path = os.path.abspath('dados')
-
-# Nome do arquivo Excel
-file_name = 'dados_jp1.xlsx'
-
-# Combinar o caminho com o nome do arquivo para obter o caminho completo
-excel_file = os.path.join(path, file_name)
-
-# Ler o Excel em um DataFrame
-cesta = pd.read_excel(excel_file)
+os.chdir("C:\\Users\\joaos\\Documents\\GitHub\\visualizacao_cesta\\arquivos\\aplicativo\\dados")
 
 # Importando os dados da cesta
 #cesta = pd.read_excel(f"{root}{path}/dados_jp1.xlsx")
+cesta = pd.read_excel("dados_jp1.xlsx")
 df = cesta.iloc[:,[6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50]]
 df30 = df.tail(1)
 
