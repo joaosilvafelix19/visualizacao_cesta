d45a53c0a99b9c378c4b7cebb8bc3c2cf28ec433
João Felix <joaosilvafelix19@gmail.com>
2024-07-23 20:48:37 -0300

    Update Cesta.py

--- arquivos/aplicativo/pages/Cesta.py
+++ arquivos/aplicativo/pages/Cesta.py
@@ -6,16 +6,12 @@ import plotly.graph_objects as go
 
 # Importando os dados e fazendo algumas manipulações
 
-# Definindo diretório
-root = os.getcwd()
-if root[0] == '/':
-    root = '/GitHub/visualizacao_cesta/'
-else:
-    root = os.path.abspath('../..')
-path = '/arquivos/aplicativo/dados'
+# URL of the Excel file on GitHub
+url = "https://raw.githubusercontent.com/joaosilvafelix19/visualizacao_cesta/main/arquivos/aplicativo/dados/cesta.csv"
 
-# Importando os dados da cesta
-cesta = pd.read_excel(f"{root}{path}/dados_jp1.xlsx")
+# Importing the data directly from GitHub
+cesta = pd.read_csv(url)
+
 df = cesta.iloc[:,[6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50]]
 df30 = df.tail(1)
 
