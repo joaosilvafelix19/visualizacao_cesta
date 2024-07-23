39a375e60247a6a6155866d697bb732e15dc9e03
João Felix <joaosilvafelix19@gmail.com>
2024-07-22 21:11:55 -0300

    Update Cesta1.py

--- arquivos/aplicativo/pages/Cesta1.py
+++ arquivos/aplicativo/pages/Cesta1.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import pandas as pd
+import requests
+import io
 import plotly.express as px
 import plotly.graph_objects as go
+
 
 # URL to the raw Excel file on GitHub
 data_url = "https://github.com/joaosilvafelix19/visualizacao_cesta/blob/main/arquivos/aplicativo/dados/dados_jp1.xlsx"
 
-# Read the Excel file
-cesta = pd.read_excel(data_url)
+# Download the file from GitHub
+response = requests.get(data_url)
+response.raise_for_status()  # Check that the request was successful
+
+# Read the Excel file into a DataFrame
+cesta = pd.read_excel(io.BytesIO(response.content))
 
 # Perform your data manipulations here
 # Example: Display the first few rows of the dataset
 st.write(cesta.head())
-
