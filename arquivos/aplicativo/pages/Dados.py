1fdff7a5ddb3f3983b1c448b7c83177fcb3b8ea1
João Felix <joaosilvafelix19@gmail.com>
2024-07-23 21:10:09 -0300

    Update Dados.py

--- arquivos/aplicativo/pages/Dados.py
+++ arquivos/aplicativo/pages/Dados.py
@@ -1,18 +1,19 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-import os
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
-from datetime import datetime, timezone
 
 url_cesta = "https://raw.githubusercontent.com/joaosilvafelix19/visualizacao_cesta/main/arquivos/aplicativo/dados/cesta.csv"
 url_precos = "https://raw.githubusercontent.com/joaosilvafelix19/visualizacao_cesta/main/arquivos/aplicativo/dados/precos.csv"
 
-# Importando os dados dos preços não ponerados
+# Importando os dados dos preços não ponderados
 precos = pd.read_csv(url_precos).round(2)
+precos['data'] = pd.to_datetime(precos['data'])  # Convertendo para datetime
 precos['data'] = precos['data'].apply(lambda x: x.strftime('%d-%m-%Y'))
+
 precos_ponderados = pd.read_csv(url_cesta).round(2)
+precos_ponderados['data'] = pd.to_datetime(precos_ponderados['data'])  # Convertendo para datetime
 precos_ponderados['data'] = precos_ponderados['data'].apply(lambda x: x.strftime('%d-%m-%Y'))
 
 st.title("Download dos dados")
@@ -20,21 +21,20 @@ st.title("Download dos dados")
 st.markdown("Abaixo, há a possibilidade de fazer o download dos dados não ponderados (Preços) e dos dados ponderados (Preços Ponderados). Os dados não ponderados referem-se ao preços coletados nos supermercados mas sem as ponderações definidas pelo decreto lei nº 399/1938, já os preços ponderados são os preços coletados e multiplicados pelos pesos descritos pelo decreto lei nº 399/1938. Para mais detalhes sobre a metodologia vá para a guia Metodologia.")
 
 # Opção de escolha entre os dados
-Opção = st.radio("Escolha uma opção para download dos dados",
-                 ('Preços', 'Preços Ponderados'))
+opcao = st.radio("Escolha uma opção para download dos dados", ('Preços', 'Preços Ponderados'))
 
 #-------------------------------------------------------------------------------------------------------------------------
 # Preços (Preços não ponderados)
 #-------------------------------------------------------------------------------------------------------------------------
 
-if Opção == "Preços":
+if opcao == "Preços":
     st.write("Você escolheu os preços não ponderados")
     
     # Dando a opção de se baixar os dados em csv
-    @st.cache
+    @st.cache_data
     def convert_df(df):
         # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return precos.to_csv().encode('utf-8')
+        return df.to_csv().encode('utf-8')
 
     csv = convert_df(precos)
 
@@ -56,14 +56,14 @@ if Opção == "Preços":
 # -------------------------------------------------------------------------------------------------------
 # Preços Ponderados 
 #--------------------------------------------------------------------------------------------------------
-if Opção == "Preços Ponderados":
+if opcao == "Preços Ponderados":
     st.write("Você escolheu os preços ponderados")
     
     # Dando a opção de se baixar os dados em csv
-    @st.cache
+    @st.cache_data
     def convert_df(df):
         # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return precos_ponderados.to_csv().encode('utf-8')
+        return df.to_csv().encode('utf-8')
 
     csv = convert_df(precos_ponderados)
 
@@ -81,4 +81,3 @@ if Opção == "Preços Ponderados":
     gridoptions = gb.build()
     AgGrid(precos_ponderados, gridOptions=gridoptions,
            allow_unsafe_jscode=True)
-    
